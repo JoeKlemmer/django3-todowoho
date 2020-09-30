7e9d42f5acdd00742f6bb5d1e6151cb477098c89
Joe Klemmer <joe.klemmer@gmail.com>
2020-09-30 13:48:20 -0400

    Added login ability

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
     # Todo
     path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos')
